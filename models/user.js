1d263fadf4b172ad541b2482cffc24a95bbfd4c8
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-11-28 15:19:11 +0100

    added _favourites

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const userSchema = mongoose.Schema({
     name: String,
     surname: String,
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    _favourites: [{type: mongoose.Schema.Types.ObjectId, ref: "Tweet", required: false}]
 });
 
 const User = mongoose.model('User', userSchema);
