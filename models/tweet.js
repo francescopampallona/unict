77d980af3c8be77dbe9b8877e45ae9b65f8d7f68
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-11-28 17:04:11 +0100

    added _likes

--- models/tweet.js
+++ models/tweet.js
@@ -4,6 +4,7 @@ const tweetSchema = mongoose.Schema({
     _author: {type: mongoose.Schema.Types.ObjectId, required: true, index: true, ref: 'User'},
     tweet: {type: String, minlenght: 1, maxlenght: 280},
     created_at: {type: Date, default: Date.now()},
+    _likes: [{type: mongoose.Schema.Types.ObjectId, ref: "User", required: false}],
     _parentTweet: {type: mongoose.Schema.Types.ObjectId, required: false, default: null}
 });
 
