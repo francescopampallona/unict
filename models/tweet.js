9f01d26f7298a83e8b4fdd69e83f6c04ca54f868
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-11-28 02:06:38 +0100

    Add files via upload

--- models/tweet.js
+++ models/tweet.js
@@ -4,7 +4,7 @@ const tweetSchema = mongoose.Schema({
     _author: {type: mongoose.Schema.Types.ObjectId, required: true, index: true, ref: 'User'},
     tweet: {type: String, minlenght: 1, maxlenght: 280},
     created_at: {type: Date, default: Date.now()},
-    comments: {type: Array}
+    _parentTweet: {type: mongoose.Schema.Types.ObjectId, required: false, default: null}
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
