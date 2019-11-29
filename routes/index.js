f04d9855df9a44766b1f1e0a561819c336f40318
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-11-29 03:39:14 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,7 @@ router.post('/login', [
 
 router.get("/me", isAuth, function(req, res) {
   User.findOne({_id: res.locals.authInfo.userId}, "-password")
+  .populate('_favourites')
   .exec(function(err, user) {
     if(err) return res.status(500).json({error:err});
     if(!user) return res.status(404).json({message: 'User not found'})
